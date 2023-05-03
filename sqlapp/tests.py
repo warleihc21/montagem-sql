f578bb803668e9931cf1ff2bf5ad9fb80aa7d120
Warlei <warleihc21@gmail.com>
2023-05-02 22:52:23 -0300

    Efetuando testes na alteração do csv

--- sqlapp/tests.py
+++ sqlapp/tests.py
@@ -1,17 +1,24 @@
 from django.test import TestCase
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+import pandas as pd
+
 
 
 #teste de codificação
-import codecs
+tabela = pd.read_csv(r"C:\Users\junio\Downloads\TESTES SQL\TESTES SQL\BRADESCO\2642 BRADESCO 15062022(Bloqueado).csv", encoding='iso-8859-1', sep=";")
 
-filename = '2642 BRADESCO 15062022(Bloqueado).csv'
-encodings = ['utf-8', 'iso-8859-1', 'windows-1252']
+tabela['VALOR BRUTO'] = tabela['VALOR BRUTO'].replace('.', ',')
+tabela['VALOR LIQUIDO'] = tabela['VALOR LIQUIDO'].replace('.', ',')
+tabela['VALOR LANÇAMENTO'] = tabela['VALOR LANÇAMENTO'].replace('.', ',')
 
-for encoding in encodings:
-    try:
-        with codecs.open(filename, 'r', encoding=encoding) as f:
-            f.read()
-        print(f'O arquivo está codificado em {encoding}.')
-        break
-    except UnicodeDecodeError:
-        print(f'A codificação {encoding} não funciona.')
+
+tabela.to_csv(r"C:\Users\junio\Downloads\2642 BRADESCO 15062022(Bloqueado).csv", index=False)
+
+print(tabela)
