4c92ddf270e22de39adf6ca897d9c04d2318362a
Warlei <warleihc21@gmail.com>
2023-05-08 21:29:41 -0300

    ajuste do painel home

--- sqlapp/models.py
+++ sqlapp/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
 class CsvData(models.Model):
-    proposta = models.CharField(max_length=100)
-    cliente = models.CharField(max_length=100)
+    banco = models.CharField(max_length=100)
+    layout = models.CharField(max_length=100)
     cpf = models.CharField(max_length=100)
-    operacao = models.CharField(max_length=100)
-    valor_bruto = models.CharField(max_length=100)
-    valor_liquido = models.CharField(max_length=100)
-    lancamento = models.CharField(max_length=100)
-    data_lancamento = models.CharField(max_length=100)
-    data_upload = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
+    natureza_do_lancamento = models.CharField(max_length=100)
+    tipo_de_lancamento = models.CharField(max_length=100)
+    observacao = models.CharField(max_length=100)
+    bruto_contrato = models.CharField(max_length=100)
+    liquido_contrato = models.CharField(max_length=100)
+    vlr_lancamento = models.CharField(max_length=100)
+    percentual_lancamento = models.CharField(max_length=100)
+    data_da_proposta = models.CharField(max_length=100)
+    data_do_lancamento = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.proposta} - {self.cliente} - {self.cpf} - {self.operacao} - {self.valor_bruto} - {self.valor_liquido} - {self.lancamento} - {self.data_lancamento} - {self.data_upload}"
+        return f"{self.banco} - {self.layout} - {self.cpf} - {self.nome} - {self.natureza_do_lancamento} - {self.tipo_de_lancamento} - {self.observacao} - {self.bruto_contrato} - {self.liquido_contrato} - {self.vlr_lancamento} - {self.percentual_lancamento} - {self.data_da_proposta} - {self.data_do_lancamento}"
+
 
